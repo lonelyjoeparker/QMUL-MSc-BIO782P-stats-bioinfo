38943046f162a9579503ccc934d191e54f3ac0c3
lonelyjoeparker <joe@lonelyjoeparker.com>
2017-11-21 15:32:53 +0000

    additional lab material

---
+++ labs/Workshop_09_Big-data/demo/really_simple.py
@@ -0,0 +1,7 @@
+import sys
+
+if len(sys.argv) > 1:
+	print 'hello ' + sys.argv[1] + '!'
+	
+else:
+	print "hello world"
---
+++ labs/Workshop_09_Big-data/demo/simpler.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+echo found $1
+#python really_simple.py $1
+ls .
