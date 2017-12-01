4084a06713b57c516482fac33f1ba4af5840601a
Owen Miller <34044093+biomiller@users.noreply.github.com>
2017-12-01 01:03:04 +0000

    Update bt17001_docker_run.sh

--- labs/Assessment_1/Student_data/596/bt17001_docker_run.sh
+++ labs/Assessment_1/Student_data/596/bt17001_docker_run.sh
@@ -4,6 +4,6 @@ docker pull bt17001/bio782p:part_one
 docker pull bt17001/bio782p:part_two
 docker pull bt17001/bio782p:part_three
 
-docker run --rm -i part_one
-docker run --rm -i part_two
-docker run --rm -i part_three
+docker run --rm  part_one
+docker run --rm  part_two
+docker run --rm  part_three
